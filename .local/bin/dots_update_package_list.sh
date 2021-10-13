d7510b82903af195ab9c0df9997e962b29b5fc76
mknurs <mikanovic@kompot.si>
2021-10-13 23:15:27 +0200

    forgot newline

--- .local/bin/dots_update_package_list.sh
+++ .local/bin/dots_update_package_list.sh
@@ -4,7 +4,7 @@ pacman -Qqe > $DOTS_PKGS_LIST
 
 if [ -n "$($DOTS_GIT_CMD status -s $DOTS_PKGS_LIST)" ]
 then
-  printf "%s" "$(tput bold)$(tput setaf 2)Package list changed, autocommit made, please push again.$(tput sgr0)"
+  printf "%s" "$(tput bold)$(tput setaf 2)Package list changed, autocommit made, please push again.$(tput sgr0)\n"
   $DOTS_GIT_CMD commit -m "autocommit" $DOTS_PKGS_LIST
 fi
 
