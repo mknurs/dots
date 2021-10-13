ba0fdf08d62077850550f4cd5cdb1d6e5e127bd6
mknurs <mikanovic@kompot.si>
2021-10-13 23:12:27 +0200

    rename dots helper scripts for consistency

---
+++ .local/bin/dots_update_package_list.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pacman -Qqe > $DOTS_PKGS_LIST
+
+if [ -n "$($DOTS_GIT_CMD status -s $DOTS_PKGS_LIST)" ]
+then
+  printf "%s" "$(tput bold)$(tput setaf 2)Package list changed, autocommit made, please push again.$(tput sgr0)"
+  $DOTS_GIT_CMD commit -m "autocommit" $DOTS_PKGS_LIST
+fi
+
